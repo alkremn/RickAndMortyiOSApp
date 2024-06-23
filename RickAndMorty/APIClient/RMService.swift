63cf4cbdeaaf83856c4f0f49b5345b9d60dda24e
Alexey Kremnev <alkremn@gmail.com>
2024-06-22 23:49:37 -0600

    Added response model

--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -17,12 +17,43 @@ final class RMService {
     /// privatized constructor
     private init() {}
     
+    enum RMServiceError: Error {
+        case failedToCreateRequest
+        case failedToGetData
+    }
+    
     /// Send Rick and Morty API Call
     /// - Parameters:
     ///   - request: Request instance
     ///   - completion: Callback with data or error
     public func execute<T: Codable>(_ request: RMRequest, expecting type: T.Type, completion: @escaping (Result<T, Error>) -> Void) {
+        guard let urlRequest = self.request(from: request) else {
+            completion(.failure(RMServiceError.failedToCreateRequest))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: urlRequest) { data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(error ?? RMServiceError.failedToGetData))
+                return
+            }
             
+            do {
+                let decodedData = try JSONDecoder().decode(type.self, from: data)
+                completion(.success(decodedData))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+        
+    }
+    
+    private func request(from rmRequest: RMRequest) -> URLRequest? {
+        guard let url = rmRequest.url else { return nil }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = rmRequest.httpMethod
+        return request
         
     }
 }
--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -14,8 +14,13 @@ final class RMCharacterViewController: UIViewController {
         view.backgroundColor = .systemBackground
         title = "Characters"
         
-        let request = RMRequest(endpoint: .character, pathComponents: ["1"], queryParameters: [
-            URLQueryItem(name: "name", value: "rick"), URLQueryItem(name: "status", value: "alive")])
-        print(request.url)
+        RMService.shared.execute(.listCharactersRequests, expecting: RMGetAllCharactersResponse.self) { result in
+            switch result {
+            case .success(let model):
+                print(model.info.pages)
+            case .failure(let error):
+                print(String(describing: error))
+            }
+        }
     }
 }
